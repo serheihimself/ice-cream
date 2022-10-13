3d9db7e0eae1be70d049e484dee70ac0176be052
serheihimself <sergm080@gmail.com>
2022-10-13 07:32:30 +0300

    modal commit

--- src/js/general-modal.js
+++ src/js/general-modal.js
@@ -1,14 +1,17 @@
-(() => {
-    const refs = {
-        openModalBtn: document.querySelector("[general-modal-open]"),
-        closeModalBtn: document.querySelector("[general-modal-close]"),
-        modal: document.querySelector("[general-modal]"),
-    };
+var openModalEntry = document.querySelectorAll(".btn_sign");
+var showModalEntry = document.querySelectorAll(".modal-window_entry");
+var closeModalEntry = document.querySelectorAll(".close-entry");
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+for(var i = 0; i < openModalEntry.length; i++){
+  openModalEntry[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    showModalEntry[0].classList.add("show-entry");
+  });
+};
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
-})();
+for(var i = 0; i < closeModalEntry.length; i++){
+  closeModalEntry[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    showModalEntry[0].classList.remove("show-entry");
+  });
+};
