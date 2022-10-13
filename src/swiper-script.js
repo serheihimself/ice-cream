261ee8178fd85fc94949bf3a336e66efc855ca36
Veronika0136 <Artemova331@gmail.com>
2022-10-13 19:24:38 +0300

    Update swiper-script.js

--- src/swiper-script.js
+++ src/swiper-script.js
@@ -2,5 +2,18 @@ let swiper = new Swiper('.mySwiper', {
   pagination: {
     el: '.swiper-pagination',
     dynamicBullets: true,
+    clickable: true,
   },
+
+  autoplay:{
+    delay:4000,
+    speed:4000,
+    },
+
+  
+  effect: 'flip',
+    flipEffect:{
+    slideShadows:true,
+    limitRotation:true,},  
+
 });
