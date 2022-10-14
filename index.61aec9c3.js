4bb13fbb301db1037bad7d971eb79ffe6b0d9fd6
serheihimself <109587036+serheihimself@users.noreply.github.com>
2022-10-14 06:27:46 +0000

    Deploying to gh-pages from @ serheihimself/ice-cream@ae406ba89c1aa94c4531b12c65c00597e12b550c 🚀

--- index.61aec9c3.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",dynamicBullets:!0}});
-//# sourceMappingURL=index.61aec9c3.js.map
---
+++ index.853417e1.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",dynamicBullets:!0,clickable:!0},autoplay:{delay:4e3,speed:4e3},effect:"flip",flipEffect:{slideShadows:!0,limitRotation:!0}});
+//# sourceMappingURL=index.853417e1.js.map
